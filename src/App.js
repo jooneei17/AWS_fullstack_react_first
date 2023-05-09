9bad5ef1f3c59955baff91baca7eaa946dba0390
joon <yijiyoon06@gmail.com>
2023-05-09 12:31:22 +0900

    useReducer 사용하여 기존의 Counter 컴포넌트 수정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import Info from "./Info";
 import Counter from "./Counter";
-
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
-  return <Counter />;
+  // const [visible, setVisible] = useState(false);
+  return (
+    // <div>
+    //   <button onClick={() => setVisible(!visible)}>
+    //     {visible ? "숨기기" : "보이기"}
+    //   </button>
+    //   <hr />
+    //   {visible && <Info />}
+    // </div>
+    <Counter />
+  );
 };
 
 export default App;
---
+++ src/Info.js
@@ -0,0 +1,40 @@
+import React, { useReducer } from "react";
+
+function reducer(state, action) {
+  return {
+    ...state,
+    [action.name]: action.value,
+  };
+}
+
+const Info = () => {
+  const [state, dispatch] = useReducer(reducer, { name: "", nickname: "" });
+  const [name, nickname] = state;
+  //     const [name, setName] = useState("");
+  //   const [nickname, setNickname] = useState("");
+  //   const onChangeName = (e) => {
+  //     setName(e.target.value);
+  //   };
+  //   const onChangeNickname = (e) => {
+  //     setNickname(e.target.value);
+  //   };
+  const onChange = (e) => dispatch(e.target);
+  return (
+    <div>
+      <div>
+        <input value={name} onChange={onChange} />
+        <input value={nickname} onChange={onChange} />
+      </div>
+      <div>
+        <div>
+          <b>이름: </b> {name}
+        </div>
+        <div>
+          <b>닉네임: </b> {nickname}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Info;
