f90f32a3e120a0d5dc50ed6cbf7d31c26108a127
joon <yijiyoon06@gmail.com>
2023-05-11 15:36:42 +0900

    뉴스 뷰어 만들기(styled-component 설치, 데이터 연동하기, 카테고리 구현하기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+// import { useState } from 'react';
+// import axios from 'axios';
 
-function App() {
+// function App() {
+//   const [data, setData] = useState(null);
+//   const onClick = async () => {
+//     // axios
+//     //   .get('https://jsonplaceholder.typicode.com/todos/1')
+//     //   .then((response) => {
+//     //     setData(response.data);
+//     //   });
+//     const response = await axios.get(
+//       'https://newsapi.org/v2/top-headlines?country=kr&apiKey=acf9e73561524f419e8879c500be81f1',
+//     );
+//     setData(response.data);
+//   };
+//   return (
+//     <div>
+//       <div>
+//         <button onClick={onClick}>불러오기</button>
+//       </div>
+//       {data && (
+//         <textarea
+//           rows={7}
+//           value={JSON.stringify(data, null, 2)}
+//           readOnly={true}
+//         />
+//       )}
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from 'react';
+import NewsList from './components/NewsList';
+import Categories from './components/Categories';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Categories />
+      <NewsList />;
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/components/NewsList.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react';
+import NewsItem from './NewsItem';
+import styled from 'styled-components';
+import axios from 'axios';
+
+const NewListBlock = styled.div`
+  box-sizing: border-box;
+  padding-bottom: 3rem;
+  width: 768px;
+  margin: 0 auto;
+  margin-top: 2rem;
+  @media (max-width: 768px) {
+    width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+`;
+
+const sampleArticle = {
+  title: '제목',
+  description: '내용',
+  url: 'https://google.com',
+  urlToImage: 'http://via.placeholder.com/160',
+};
+
+const NewsList = () => {
+  const [articles, setArticles] = useState(null);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    //async를 사용하는 함수를 별도로 선언
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get(
+          'https://newsapi.org/v2/top-headlines?country=us&apiKey=acf9e73561524f419e8879c500be81f1',
+        );
+        setArticles(response.data.articles);
+      } catch (e) {
+        console.log(e);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  // 대기 중일 때
+  if (loading) {
+    return <NewListBlock>대기 중 ....</NewListBlock>;
+  }
+  // 아직 articles 값이 설정되지 않았을 때
+  if (!articles) {
+    return null;
+  }
+
+  //articles 값이 유효할 때
+  return (
+    <NewListBlock>
+      {articles.map((article) => (
+        <NewsItem key={article.url} article={article} />
+      ))}
+    </NewListBlock>
+  );
+};
+
+export default NewsList;
